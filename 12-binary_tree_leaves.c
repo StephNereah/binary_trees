948c4c9010d27bab1eb8d98706a71c8709932ed0
StephNereah <stphnadhiambo@gmail.com>
2024-03-16 06:46:02 -0700

    12.Leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,21 @@
+#include "binary_trees"
+
+/**
+ * binary_trees_leaves - gets the # of leaves of a binary tree
+ * @tree: root node to draw height from tree
+ *
+ * Return: size_t representing height, 0/ NULL on failure
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+	leaves += binary_tree_leaves(tree->left);
+	leaves += binary_tree_leaves(tree->right);
+	return (leaves);
+}
